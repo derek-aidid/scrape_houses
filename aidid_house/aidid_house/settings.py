3f910c8d892cd5bdeaed086b7958bdab74184af1
dereksun105 <74845705+dereksun105@users.noreply.github.com>
2025-01-21 11:18:15 +0800

    Update settings.py

--- aidid_house/aidid_house/settings.py
+++ aidid_house/aidid_house/settings.py
@@ -105,5 +105,3 @@ DOWNLOAD_FAIL_ON_DATALOSS = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-
-LOG_STDOUT=False
