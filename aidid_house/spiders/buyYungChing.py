5bc055cb7f965c4ba83cb18db8790a946c5fcafd
dereksun105 <74845705+dereksun105@users.noreply.github.com>
2025-05-10 09:45:39 +0800

    Update buyYungChing.py

--- aidid_house/spiders/buyYungChing.py
+++ aidid_house/spiders/buyYungChing.py
@@ -42,7 +42,7 @@ class BuyyongchingSpider(scrapy.Spider):
             yield scrapy.Request(
                 f"{response.url}/?pg={i}",
                 callback=self.parse_list_page,
-                meta={"proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10001"}
+                meta={"proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10000"}
             )
 
     def parse_list_page(self, response):
@@ -52,7 +52,7 @@ class BuyyongchingSpider(scrapy.Spider):
             yield scrapy.Request(
                 full_url,
                 callback=self.parse_case_page,
-                meta={"proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10001"}
+                meta={"proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10000"}
             )
 
     def parse_case_page(self, response):
@@ -103,7 +103,7 @@ class BuyyongchingSpider(scrapy.Spider):
                 poi_api_url,
                 callback=self.parse_poi_data,
                 headers=headers,
-                meta={"item": item, "proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10001"}
+                meta={"item": item, "proxy": "https://derek5g:IpRtT=9qS797renxnk@dc.decodo.com:10000"}
             )
 
         except Exception as e:
